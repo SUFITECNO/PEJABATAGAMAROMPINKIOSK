6cdf334f3f03974012ed0c65472feec85868a422
SUFITECNO <admin@tecno.com.my>
2026-06-09 12:09:35 +0800

    UPDATE

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# PEJABATAGAMAROMPINKIOSK
-A kiosk queue management system for PEJABAT AGAMA ISLAM that generates queue numbers and displays customer calls for efficient service management.
+# PEJABATAGAMAKIOSK
+Kiosk system for PEJABAT AGAMA ISLAM
---
+++ firebase.js
@@ -0,0 +1,14 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBQp3ZT3No_nkBLPgMKhHjQZ2YkbRq9-mg",
+  authDomain: "queue-system-c4615.firebaseapp.com",
+  databaseURL: "https://queue-system-c4615-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "queue-system-c4615",
+  storageBucket: "queue-system-c4615.firebasestorage.app",
+  messagingSenderId: "291773115991",
+  appId: "1:291773115991:web:f98c7d8c54993f2fc5bd12",
+  measurementId: "G-8PTN1K3XSG"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
